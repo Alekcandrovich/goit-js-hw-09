3ae96a837b65deced15a84b7913f9cae76954315
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-04-07 18:34:21 +0300

    9 - 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+let intervalId = null;
+
+function onStartClick() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+function onStopClick() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(intervalId);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
